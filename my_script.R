903f613fb966d8f78c6f020e851957ce9c830b03
BenardOmido <benomido2001@gmail.com>
2024-11-07 11:26:55 +0300

    Calculated average mpg per am

--- my_script.R
+++ my_script.R
@@ -6,3 +6,9 @@ ggplot(mtcars, aes(x = mpg, y = hp)) +
   geom_point() +
   labs(x = "Miles Per Gallon (mpg)", 
        y = "Hp")
+
+library(dplyr)
+
+mtcars %>% 
+  group_by(am) %>% 
+  summarize(mean_mpg = mean(mpg))
