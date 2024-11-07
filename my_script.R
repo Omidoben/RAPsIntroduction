d2cc031f69da7604c9aabbd6908bafb5a1640c9c
BenardOmido <benomido2001@gmail.com>
2024-11-07 11:43:13 +0300

    More models

--- my_script.R
+++ my_script.R
@@ -7,8 +7,13 @@ ggplot(mtcars, aes(x = mpg, y = hp)) +
   labs(x = "Miles Per Gallon (mpg)", 
        y = "Hp")
 
+
+rf2 <- randomForest(hp ~ mpg + am + cyl, data = mtcars)
+plot(rf2)
+
 library(dplyr)
 
 mtcars %>% 
   group_by(am) %>% 
   summarize(mean_mpg = mean(mpg))
+
